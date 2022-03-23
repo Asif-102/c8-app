4b30e62dd475744ce300aed69dd3123322371bf2
Asif-102 <aiqbal517@gmail.com>
2022-03-23 23:49:27 +0600

    initial work done

--- client/src/App.js
+++ client/src/App.js
@@ -9,34 +9,40 @@ function App() {
 
   const [userName, setUserName] = React.useState("")
   const [room, setRoom] = React.useState("")
+  const [showChat, setShowChat] = React.useState(false);
 
-  const joinRoom = ()=>{
-    if(userName && room)
-    {
+  const joinRoom = () => {
+    if (userName && room) {
       socket.emit("join_room", room);
+      
+      setShowChat(true);
     }
   }
 
   return (
     <div className="App">
 
-      <h3>Join a Chat</h3>
+      {
+        !showChat ?
+          <div>
+            <h3>Join a Chat Room</h3>
 
-      <input 
-      type="text" 
-      placeholder="John..." 
-      onChange={(event)=> setUserName(event.target.value)}
-      />
+            <input
+              type="text"
+              placeholder="John..."
+              onChange={(event) => setUserName(event.target.value)}
+            />
 
-      <input 
-      type="text" 
-      placeholder="Room ID..." 
-      onChange={(event)=> setRoom(event.target.value)}
-      />
+            <input
+              type="text"
+              placeholder="Room ID..."
+              onChange={(event) => setRoom(event.target.value)}
+            />
 
-      <button onClick={joinRoom}>Join A Room</button>
-
-      <Chat socket={socket} userName={userName} room={room}/>
+            <button onClick={joinRoom}>Join A Room</button>
+          </div> :
+          <Chat socket={socket} userName={userName} room={room} />
+      }
 
     </div>
   );
--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Chat = ({ socket, userName, room }) => {
 
     const [typedMessage, setTypedMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
 
     const sendMessage = async ()=>{
         if(typedMessage)
@@ -11,21 +12,36 @@ const Chat = ({ socket, userName, room }) => {
                 room,
                 author: userName,
                 message: typedMessage,
-                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+                time: new Date(Date.now()).getHours() + ":" 
+                + new Date(Date.now()).getMinutes()
             }
 
             await socket.emit("send_message", messageData);
+            setMessageList((list)=> [...list, messageData]);
+            setTypedMessage("")
         }
     }
+
+    useEffect(()=>{
+        socket.on("receive_message", (data)=>{
+            // console.log(data);
+            setMessageList((list)=> [...list, data]);
+        });
+    },[socket]);
 
     return (
         <div>
             <div className='chat-header'>
                 <p>Let's Chat</p>
             </div>
-            <div className='chat-body'></div>
+            <div className='chat-body'>
+                {
+                    messageList.map((messageData, index)=> <h1 key={index}>{messageData.message}</h1>)
+                }
+            </div>
             <div className='chat-footer'>
                 <input 
+                value={typedMessage}
                 type="text" 
                 placeholder="Hey....." 
                 onChange={(event)=> setTypedMessage(event.target.value)}
