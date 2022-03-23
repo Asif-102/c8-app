74a1b60ab724fd04a735a2c2a6a4ac864b5f89ff
Asif-102 <aiqbal517@gmail.com>
2022-03-23 22:37:43 +0600

    collect message data from client to server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -13,6 +13,8 @@ const Chat = ({ socket, userName, room }) => {
                 message: typedMessage,
                 time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
             }
+
+            await socket.emit("send_message", messageData);
         }
     }
 
@@ -28,7 +30,7 @@ const Chat = ({ socket, userName, room }) => {
                 placeholder="Hey....." 
                 onChange={(event)=> setTypedMessage(event.target.value)}
                 />
-                <button>&#9658;</button>
+                <button onClick={sendMessage}>&#9658;</button>
             </div>
         </div>
     )
