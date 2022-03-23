897c6bfa60a1399313a97362a654dd64bdb339d4
Asif-102 <aiqbal517@gmail.com>
2022-03-23 21:57:55 +0600

    initial user input configure complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
+import React from "react";
 import './App.css';
 import io from 'socket.io-client';
 
 const socket = io.connect("http://localhost:5000");
 
 function App() {
+
+  const [userName, setUserName] = React.useState("")
+  const [room, setRoom] = React.useState("")
+
+  const joinRoom = ()=>{
+    if(userName && room)
+    {
+      console.log(userName, room);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+
+      <h3>Join a Chat</h3>
+
+      <input 
+      type="text" 
+      placeholder="John..." 
+      onChange={(event)=> setUserName(event.target.value)}
+      />
+
+      <input 
+      type="text" 
+      placeholder="Room ID..." 
+      onChange={(event)=> setRoom(event.target.value)}
+      />
+
+      <button onClick={joinRoom}>Join A Room</button>
+
     </div>
   );
 }
